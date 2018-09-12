2d20cde0c66f4adac07d38a157a5d96fe3932c79
Edvinas K <edvinas.kesminas@gmail.com>
2018-09-12 14:06:16 +0300

    SummarizingDomainProductCostNameConstructors

--- EShopPrototypeConsole.Domain/Product.cs
+++ EShopPrototypeConsole.Domain/Product.cs
@@ -8,8 +8,30 @@ namespace EShopPrototypeConsole.Domain
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        /// <summary>
+        /// Create a product with default name and specified cost.
+        /// </summary>
+        /// <param name="cost">The cost or the price of the product ex: 99.99m</param>
+        /// <returns>Returns a new product with specified cost and default name</returns>
+        public Product(decimal cost)
+        {
+            Cost = cost;
+        }
+
+        /// <summary>
+        /// Create a product with specified name and cost.
+        /// </summary>
+        /// <param name="name">Name or the title of the product ex: "Samsung S8"</param>
+        /// <param name="cost">The cost or the price of the product ex: 99.99m</param>
+        /// <returns>Returns a new product with specified cost and name</returns>
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "Default";
         public decimal Cost { get; set; }
     }
 }
--- EShopPrototypeConsole/Program.cs
+++ EShopPrototypeConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EShopPrototypeConsole.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace EShopPrototypeConsole
     {
         static void Main(string[] args)
         {
+            var product1 = new Product("TestProduct", 99.99m);
+
             Console.WriteLine("Hello world!");
             Console.ReadLine();
         }
