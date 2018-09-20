221eae5d598b69ff8f4c8e1cde926f4d05375993
Edvinas K <edvinas.kesminas@gmail.com>
2018-09-20 12:03:44 +0300

    AddingDataLayerAndCartProductRepositories

---
+++ EShopPrototypeConsole.Data/CartRepository.cs
@@ -0,0 +1,16 @@
+﻿using EShopPrototypeConsole.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace EShopPrototypeConsole.Data
+{
+    public class CartRepository
+    {
+        public CartRepository()
+        {
+            Carts = new List<Cart>();
+        }
+
+        public ICollection<Cart> Carts { get; set; }
+    }
+}
---
+++ EShopPrototypeConsole.Data/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using EShopPrototypeConsole.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopPrototypeConsole.Data
+{
+    public class ProductRepository
+    {
+        public ProductRepository()
+        {
+            Products = new List<Product>();
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+
+    }
+}
